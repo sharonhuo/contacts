86a866e428ee153778034d80e40f58fc58fbae49
Sharon <sharon@Sharons-MacBook-Pro.local>
2017-07-09 22:25:28 +1000

    Test commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts';
 
+const contacts = [
+  {
+    "id": "ryan",
+    "name": "Ryan Florence",
+    "email": "ryan@reacttraining.com",
+    "avatarURL": "http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id": "michael",
+    "name": "Michael Jackson",
+    "email": "michael@reacttraining.com",
+    "avatarURL": "http://localhost:5001/michael.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "email": "tyler@reacttraining.com",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+];
 
 class App extends Component {
 
@@ -7,7 +28,7 @@ class App extends Component {
   render() {
     return (
       <div>
-         <h1>hello world!</h1>
+         <ListContacts contacts={contacts}/>
       </div>
     )
   }
