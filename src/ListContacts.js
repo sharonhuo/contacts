f4615fa3bfed0e9c6193449e6c332047f2acca08
sharon <sharonhuo2011@gmail.com>
2017-07-13 09:05:19 +1000

    Added search box

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class ListContacts extends Component {
-  render() {
-    console.log('this.props', this.props);
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  render () {
     return (
-      <div>
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="search text"
+            vaue={this.state.query}
+          />
+        </div>
         <ol className='contact-list'>
           {this.props.contacts.map( (contact) => (
               <li key={contact.id} className='contact-list-item'>
@@ -16,14 +33,13 @@ class ListContacts extends Component {
                 <p>{contact.name}</p>
                 <p>{contact.email}</p>
               </div>
-              <button className='contact-remove'>Remove</button>
+              <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>Remove</button>
               </li>
             ))
           }
         </ol>
       </div>
-    )
+    )}
   }
-}
 
 export default ListContacts
